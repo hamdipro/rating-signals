f8a70ff7c8a1f75c6b773026c57b98b42e1d3af7
hamdi.hmidi <hmidi@gmail.com>
2024-06-04 09:16:47 +0200

    full rating app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,43 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'rating-app';
+
+
+  user_rating = signal<number>(4);
+  rating_type = signal<number>(0); // 0 : STARS ; 1: THUMBS
+
+  empty_icon = computed(()=> {
+    return this.rating_type() === 0 ? 'star_empty.png' : 'thumb_empty.png';
+  });
+
+
+  filled_icon = computed(()=> {
+    return this.rating_type() === 0 ? 'star_filled.png' : 'thumb_filled.png';
+  });
+
+  title = 'Simple rating app';
+
+  controlDown: string = "arrow_left.png";
+  controlUp: string = "arrow_right.png";
+
+  constructor(){
+    effect(() => {
+      console.log('user rating has changed to : ', this.user_rating());   
+      console.log('rating type has changed to : ', this.rating_type());   
+    });
+  }
+
+
+  chooseRating(index: number) : void {
+    this.user_rating.set(index + 1);
+  }
+
+  rateUp() {
+    this.user_rating.update((val) => val === 5 ? 5 : val + 1);
+  }
+  rateDown() {
+    this.user_rating.update((val) => val === 1 ? 1 : val - 1);
+  }
+
+  
 }
